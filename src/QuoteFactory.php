d891cfca4561430d5135eae9bf5c6e87f0acdfca
Alex Macdonald-Smith <alex.macsmith@gmail.com>
2021-04-11 08:59:53 -0400

    Jocko quote package and tests

---
+++ src/QuoteFactory.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace AMacSmith\JockoQuotes;
+
+use Exception;
+use GuzzleHttp\Client;
+use RuntimeException;
+
+class QuoteFactory
+{
+    const QUOTE_API_ENDPOINT = 'https://www.jqdb.org/quotes?q=';
+    const QUOTE_COUNT_API_ENDPOINT = 'https://www.jqdb.org/quotes?q=COUNT';
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * JokeFactory constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client = null)
+    {
+        $this->client = $client ?: new Client();
+    }
+
+    public function getQuoteCount()
+    {
+        $response = $this->client->get(self::QUOTE_COUNT_API_ENDPOINT);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getRandomQuote()
+    {
+        $quoteCount = $this->getQuoteCount();
+        $randomQuoteIndex = mt_rand(0, $quoteCount);
+
+        $response = $this->client->get(self::QUOTE_API_ENDPOINT . "-$randomQuoteIndex");
+
+        return json_decode($response->getBody()->getContents())[0]->q;
+    }
+
+    public function getQuoteByInteger(int $quoteIndex)
+    {
+        $quoteCount = $this->getQuoteCount();
+
+        if($quoteIndex < 0 || $quoteIndex > $quoteCount) {
+            throw new RuntimeException('The Quote Index passed is not within the valid Quote Index Range 0 to ' . $quoteCount);
+        }
+
+        $quoteIndexString = "-{$quoteIndex}";
+
+        var_dump($quoteIndexString);
+
+        $response = $this->client->get(self::QUOTE_API_ENDPOINT . $quoteIndexString);
+
+        return json_decode($response->getBody()->getContents())[0]->q;
+    }
+}
---
+++ tests/QuoteFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace AMacSmith\JockoQuotes\Tests;
+
+use AMacSmith\JockoQuotes\QuoteFactory;
+use PHPUnit\Framework\TestCase;
+
+class QuoteFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_quote()
+    {
+        $quotes = new QuoteFactory();
+
+        $quote = $quotes->getRandomQuote();
+
+        $this->assertIsString($quote);
+    }
+
+    /** @test */
+    public function it_returns_a_quote_given_an_integer_index()
+    {
+        $quotes = new QuoteFactory();
+
+        $quote = $quotes->getQuoteByInteger(1);
+
+        var_dump($quote);
+
+        $this->assertEquals("I'm honored that I had the privilege to go and fight for America. — Thom \"DRAGO\" Dzieran", $quote);
+    }
+}
